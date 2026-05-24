950d8a031a399406003611e7f0b6e446eb699b16
RhianMae <opalla.rhian@gmail.com>
2026-05-24 21:16:33 +0800

    Update bastamaonani1.py
    "add_student" function added

--- bastamaonani1.py
+++ bastamaonani1.py
@@ -5,3 +5,13 @@ try:
         print("Student file created!")
 except FileExistsError:
     print("Student file already exists!")
+
+def add_student():
+    student_id = input("Enter Student ID: ")
+    name = input("Enter Name: ")
+    course = input("Enter Course: ")
+
+    with open(FILENAME, "a") as file:
+        file.write(f"{student_id},{name},{course}\n")
+
+    print("Student added successfully!")
